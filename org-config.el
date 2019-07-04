c57ef3f98865ef8272f1297075f5eef324994082
Justin <justinj@thoughtworks.com>
2019-07-04 17:31:10 +0530

    Added texlive bin path for export to latex pdf

--- org-config.el
+++ org-config.el
@@ -1,20 +1,24 @@
 (require 'org)
+(setenv "PATH" (concat (getenv "PATH") ":/Library/TeX/texbin/"))
 (setq org-directory "~/Documents/org-notes")
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 (add-hook 'org-mode-hook 'org-zotxt-mode)
 (add-hook 'org-mode-hook 'org-indent-mode)
-(setq org-agenda-files '("~/Documents/org-notes/gtd/agenda.org"))
-(setq org-default-notes-file (concat org-directory "/gtd/capture.org"))
+(setq org-agenda-files '("~/Documents/org-notes/projects/agenda.org"
+			 "~/Documents/org-notes/projects/inbox.org"
+                         "~/Documents/org-notes/projects/projects.org"
+                         "~/Documents/org-notes/projects/tickler.org"))
+(setq org-default-notes-file (concat org-directory "/projects/capture.org"))
 
 (setq org-todo-keywords '((sequence "TODO(t)" "WORKING(w)" "|" "DONE(d)" "CANCELLED(c)")))
 
 (setq org-capture-templates '(("t" "Todo [inbox]" entry
-                               (file+headline "~/Documents/org-notes/gtd/inbox.org" "Tasks")
+                               (file+headline "~/Documents/org-notes/projects/inbox.org" "Tasks")
                                "* TODO %i%?")
                               ("T" "Tickler" entry
-                               (file+headline "~/Documents/org-notes/gtd/tickler.org" "Tickler")
+                               (file+headline "~/Documents/org-notes/projects/tickler.org" "Tickler")
                                "* %i%? \n %U")))
 
-(setq org-refile-targets '(("~/Documents/org-notes/gtd/gtd.org" :maxlevel . 3)
-                           ("~/Documents/org-notes/gtd/someday.org" :level . 1)
-                           ("~/Documents/org-notes/gtd/tickler.org" :maxlevel . 2)))
+(setq org-refile-targets '(("~/Documents/org-notes/projects/projects.org" :maxlevel . 3)
+                           ("~/Documents/org-notes/projects/someday.org" :level . 1)
+                           ("~/Documents/org-notes/projects/tickler.org" :maxlevel . 2)))
